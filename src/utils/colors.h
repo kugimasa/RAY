e2f777afdf5f44100cf346ec4a272b114cbb8ab1
kugimasa <kugimania@gmail.com>
2021-12-21 01:27:56 +0900

    🔧Set C++ version to C++14

---
+++ src/utils/colors.h
@@ -0,0 +1,10 @@
+//
+// Created by kugimasa on 2021/12/21.
+//
+
+#ifndef RAY_SRC_UTILS_COLORS_H_
+#define RAY_SRC_UTILS_COLORS_H_
+
+#include "vec3.h"
+static const vec3 KUGI_COLOR(0.274, 0.886, 0.745);
+#endif //RAY_SRC_UTILS_COLORS_H_
